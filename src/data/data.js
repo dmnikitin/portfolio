80abfb392436db1d833cea83c8cd0c97472176ee
dmitry <joydnby@gmail.com>
2020-06-02 10:49:30 +0300

    refactor: add separate data file for magic values, images now lighter

---
+++ src/data/data.js
@@ -0,0 +1,15 @@
+export const projectsList = [
+	{ index: 1, name: "waffa", features: "napoli", technologies: ["React, Redux, React Transition Group"], link: "https://dmnikitin.github.io/notes/" },
+    { index: 2, name: "NOTES", features: "Create/edit/search/delete your notes with this app, edit notes content with text markdown editor, changes synchronized with browser local storage", technologies: ["React, Redux, React Transition Group"], link: "https://dmnikitin.github.io/notes/" },
+    { index: 3, name: "NEWS", features: "View the latest news from selected topics, comes with endless scroll feature", technologies: ["Backend: NodeJS", "Frontend: React, React-router, Material-ui"], link: "http://react-news-2019.herokuapp.com/" },
+    { index: 4, name: "POMODORO", features: "Pomodoro clock app with progress bar, control buttons, work/rest cycles", technologies: ["vanilla javascript"], link: "https://dmnikitin.github.io/pomodoro/" },
+    { index: 5, name: "WEATHER", features: "Shows weather based on user's current location, dayly/weekly weather view", technologies: ["Backend: NodeJS", "Frontend: vanilla javascript"], link: "https://js-weather-2019.herokuapp.com/" },
+	{ index: 6, name: "TICTAC", features: "Play the classic TicTacToe game against computer, or in hot-seat mode", technologies: ["vanilla javascript"], link: "https://dmnikitin.github.io/tictactoe/index.html" },
+	{ index: 7, name: "TICTAC", features: "Play the classic TicTacToe game against computer, or in hot-seat mode", technologies: ["vanilla javascript"], link: "https://dmnikitin.github.io/tictactoe/index.html" },
+];
+
+export const state = {
+    about: 'about',
+    works: 'works',
+    contacts: 'contacts',
+}
