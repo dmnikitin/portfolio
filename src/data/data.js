d80196d067ccdc57b830a1a6dc50b6495deccea7
dmitry <joydnby@gmail.com>
2020-06-03 11:07:22 +0300

    feat: add new images, modify data file

--- src/data/data.js
+++ src/data/data.js
@@ -1,11 +1,14 @@
-export const projectsList = [
-	{ index: 1, name: "waffa", features: "napoli", technologies: ["React, Redux, React Transition Group"], link: "https://dmnikitin.github.io/notes/" },
-    { index: 2, name: "NOTES", features: "Create/edit/search/delete your notes with this app, edit notes content with text markdown editor, changes synchronized with browser local storage", technologies: ["React, Redux, React Transition Group"], link: "https://dmnikitin.github.io/notes/" },
-    { index: 3, name: "NEWS", features: "View the latest news from selected topics, comes with endless scroll feature", technologies: ["Backend: NodeJS", "Frontend: React, React-router, Material-ui"], link: "http://react-news-2019.herokuapp.com/" },
-    { index: 4, name: "POMODORO", features: "Pomodoro clock app with progress bar, control buttons, work/rest cycles", technologies: ["vanilla javascript"], link: "https://dmnikitin.github.io/pomodoro/" },
-    { index: 5, name: "WEATHER", features: "Shows weather based on user's current location, dayly/weekly weather view", technologies: ["Backend: NodeJS", "Frontend: vanilla javascript"], link: "https://js-weather-2019.herokuapp.com/" },
-	{ index: 6, name: "TICTAC", features: "Play the classic TicTacToe game against computer, or in hot-seat mode", technologies: ["vanilla javascript"], link: "https://dmnikitin.github.io/tictactoe/index.html" },
-	{ index: 7, name: "TICTAC", features: "Play the classic TicTacToe game against computer, or in hot-seat mode", technologies: ["vanilla javascript"], link: "https://dmnikitin.github.io/tictactoe/index.html" },
+export const projectsList1 = [
+	{ index: 1, name: "CULTURE PORTAL", features: "Teamwork example, website about famous film directors from Belarus", technologies: ["Angular"], link: "https://romantic-yonath-91b28d.netlify.com/main", github:"https://github.com/PoMaKoM/Culture-Portal_Team5"},
+    { index: 2, name: "YOUTUBE CLIENT", features: "Youtube client with authentication, MEAN stack", technologies: ["Angular, NodeJS, MongoDB"], link: "https://cryptic-sierra-45688.herokuapp.com/", github:"https://github.com/dmnikitin/angular-youtube-client"},
+    { index: 3, name: "WEATHER", features: "Weather forecast for selected location, implemented with mvc pattern", technologies: ["NodeJS"], link: "https://pure-shelf-01620.herokuapp.com/ ", github:"https://github.com/dmnikitin/js-weather-mvc"},
+];
+
+export const projectsList2 = [
+    { index: 4, name: "NOTES", features: "Notes app with markdown editor", technologies: ["React, Redux"], link: "https://dmnikitin.github.io/notes/", github:"https://github.com/dmnikitin/react-notes"},
+    { index: 5, name: "NEWS", features: "News app with endless scroll feature", technologies: ["React, NodeJS"], link: "http://react-news-2019.herokuapp.com/", github:"https://github.com/dmnikitin/react-news"},
+    { index: 6, name: "POMODORO", features: "Pomodoro clock app with progress bar, cycles & controls", technologies: ["Javascript"], link: "https://dmnikitin.github.io/pomodoro/", github:"https://github.com/dmnikitin/js-pomodoro"},
+	{ index: 7, name: "TICTAC", features: "TicTacToe game: single-player, hot-seat mode", technologies: ["Javascript"], link: "https://dmnikitin.github.io/tictactoe/index.html", github:"https://github.com/dmnikitin/js-tictactoe"},
 ];
 
 export const state = {
