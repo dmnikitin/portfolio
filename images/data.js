b2eebbb8074c0e98ecf712aa8e7d0ff41701bc3c
dmitry <joydnby@gmail.com>
2020-06-03 11:28:26 +0300

    Updates

---
+++ images/data.js
@@ -0,0 +1,18 @@
+export const projectsList1 = [
+	{ index: 1, name: "CULTURE PORTAL", features: "Teamwork example, website about famous film directors from Belarus", technologies: ["Angular"], link: "https://romantic-yonath-91b28d.netlify.com/main", github:"https://github.com/PoMaKoM/Culture-Portal_Team5"},
+    { index: 2, name: "YOUTUBE CLIENT", features: "Youtube client with authentication, MEAN stack", technologies: ["Angular, NodeJS, MongoDB"], link: "https://cryptic-sierra-45688.herokuapp.com/", github:"https://github.com/dmnikitin/angular-youtube-client"},
+    { index: 3, name: "WEATHER", features: "Weather forecast for selected location, implemented with mvc pattern", technologies: ["NodeJS"], link: "https://pure-shelf-01620.herokuapp.com/ ", github:"https://github.com/dmnikitin/js-weather-mvc"},
+];
+
+export const projectsList2 = [
+    { index: 4, name: "NOTES", features: "Notes app with markdown editor", technologies: ["React, Redux"], link: "https://dmnikitin.github.io/notes/", github:"https://github.com/dmnikitin/react-notes"},
+    { index: 5, name: "NEWS", features: "News app with endless scroll feature", technologies: ["React, NodeJS"], link: "http://react-news-2019.herokuapp.com/", github:"https://github.com/dmnikitin/react-news"},
+    { index: 6, name: "POMODORO", features: "Pomodoro clock app with progress bar, cycles & controls", technologies: ["Javascript"], link: "https://dmnikitin.github.io/pomodoro/", github:"https://github.com/dmnikitin/js-pomodoro"},
+	{ index: 7, name: "TICTAC", features: "TicTacToe game: single-player, hot-seat mode", technologies: ["Javascript"], link: "https://dmnikitin.github.io/tictactoe/index.html", github:"https://github.com/dmnikitin/js-tictactoe"},
+];
+
+export const state = {
+    about: 'about',
+    works: 'works',
+    contacts: 'contacts',
+}
