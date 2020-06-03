7d1fd315c6813eb3d8d78d987eca2dffca14d5c3
dmitry <joydnby@gmail.com>
2020-06-03 11:09:56 +0300

    refactor: works component, modify styles and responsive

--- src/components/works.js
+++ src/components/works.js
@@ -1,49 +1,48 @@
 import React from 'react';
 import Flip from 'react-reveal/Flip';
 import Fade from 'react-reveal/Fade';
-
-import Instance from "./instance/instance.js";
+import Instance from "./instance.js";
 import Link from "./link.js";
 import Bars from "./bars.js";
+import { state, projectsList1, projectsList2 } from '../data/data';
 
-const projectsList = [
-	{index: 1, name: "NOTES", features: "Create/edit/search/delete your notes with this app, edit notes content with text markdown editor, changes synchronized with browser local storage", technologies: ["React, Redux, React Transition Group"], link: "https://dmnikitin.github.io/notes/" },
-	{index: 2, name: "NEWS", features: "View the latest news from selected topics, comes with endless scroll feature", technologies: ["Backend: NodeJS", "Frontend: React, React-router, Material-ui"], link : "http://react-news-2019.herokuapp.com/"},
-	{index: 3, name: "POMODORO", features: "Pomodoro clock app with progress bar, control buttons, work/rest cycles", technologies: ["vanilla javascript"], link: "https://dmnikitin.github.io/pomodoro/"},
-	{index: 4, name: "WEATHER", features: "Shows weather based on user's current location, dayly/weekly weather view", technologies: ["Backend: NodeJS", "Frontend: vanilla javascript"], link: "https://js-weather-2019.herokuapp.com/" },
-	{index: 5, name: "TICTAC", features: "Play the classic TicTacToe game against computer, or in hot-seat mode", technologies: ["vanilla javascript"], link: "https://dmnikitin.github.io/tictactoe/index.html"},
-];
+const { works, about, contacts } = state;
 
-const Works = (props) =>
-		
-		<div className="works">
-				<Fade left>
-					<div className="works-top">
-						<div className="works-text">
-						    <div className="works-name">
-						    	<div>recent works</div>
-						     </div> 
-						    <div className="works-links">
-						     	<Link tag="div" direction = "about" changePage={props.changePage}/>
-								<Link tag="div" direction = "contacts" changePage={props.changePage}/>
-						    </div>
-						    <Bars name="works" changePage={props.changePage}/>
-
-						</div>					
+const Works = ({changePage}) =>
+    <div className="works">
+		<Fade left>
+			<div className="works-top">
+				<div className="works-text">
+					<div className="works-name">
+						<div>recent works</div>
+					</div> 
+					<div className="works-links">
+						<Link tag="button" direction={about} changePage={changePage}/>
+						<Link tag="button" direction={contacts} changePage={changePage}/>
 					</div>
-				</Fade>
-				<div className="works-main">
-			 		<Flip left >
-            		 	{projectsList.map( el => {
-							let cName = `instance-${el.index}`
-							let currentInstance = 					  
-			 						<Instance name={cName} content={el} key={el.index}/> 				
-							    return currentInstance 
-			           		})}	       		
-					</Flip>
-        		</div>	
-		</div>
-	 	
+					<Bars name={works} changePage={changePage}/>
+				</div>					
+			</div>
+		</Fade>
+		<div className="works-main">
+			<div className="works-main-part">
+				<Flip left>
+					{projectsList1.map( el => {
+						const cName = `instance-${el.index}`
+						return <Instance name={cName} content={el} key={el.index}/>						
+					})}	       		
+				</Flip>
+
+			</div>
+			<div className="works-main-part">
+				<Flip right>
+					{projectsList2.map( el => {
+						const cName = `instance-${el.index}`
+						return <Instance name={cName} content={el} key={el.index}/>						
+					})}	       		
+				</Flip>
+			</div>
+		</div>	
+	</div>
 
 export default Works;
-
