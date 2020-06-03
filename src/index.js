8e6908805bbfdd984726a1b11f6950cd914a22c8
dmitry <joydnby@gmail.com>
2020-06-03 11:08:17 +0300

    refactor: index js, basic styles file

--- src/index.js
+++ src/index.js
@@ -4,30 +4,48 @@ import './sass/styles.scss';
 import Works from './components/works.js';
 import About from './components/about.js';
 import Contact from './components/contact.js';
+import { state } from './data/data';
+
+const { works, about, contacts } = state;
 
 export default class Root extends Component {
 	
 	componentDidMount() {
-		fetch('https://js-weather-2019.herokuapp.com/loadserver');
+		fetch('https://pure-shelf-01620.herokuapp.com/ ');
 		fetch('https://react-news-2019.herokuapp.com/loadserver');
-		fetch('https://stormy-chamber-60382.herokuapp.com');
+		fetch('https://cryptic-sierra-45688.herokuapp.com/');
 	}
 
-	state = {displayed: "about"}
+	state = {displayed: works}
 
 	changePage = (value) => this.setState({displayed: value})
 
 	render() {
 		let mainbox;
-		if (this.state.displayed === "about") {mainbox = <About changePage={this.changePage}/>} else if (this.state.displayed === "works") {mainbox = <Works changePage={this.changePage}/>} else if (this.state.displayed === "contacts") {mainbox = <Contact changePage={this.changePage} />}
-		
-		return (			
-			    	<div className="main">
-												
-						{mainbox}	
-						<div className="footer"> © dmitry nikitin, 2018-2019 </div>	
-					</div>		
-			);
+		switch (this.state.displayed) {
+			case about: {
+				mainbox = <About changePage = {this.changePage}/>
+				break;
+			}
+			case works: {
+				mainbox = <Works changePage = {this.changePage}/>
+				break;
+			}
+			case contacts: {
+				mainbox = <Contact changePage = {this.changePage}/>
+				break;
+			}
+			default: {
+				mainbox = <About changePage = {this.changePage}/>
+			}
+		}
+			
+		return (
+			<div className="main">												
+				{mainbox}	
+				<div className="footer"> © dmitrij nikitin, 2018-2020 </div>	
+			</div>		
+		);
 	}
 }
 
