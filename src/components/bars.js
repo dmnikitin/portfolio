db4a42f8d2d2c41a483b9d3c5746b208f17d33a9
dmitry <joydnby@gmail.com>
2020-06-04 20:10:53 +0300

    refactor: small changes in bars and mixin file

--- src/components/bars.js
+++ src/components/bars.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Link from "./link.js";
 import Fade from 'react-reveal/Fade';
+import { state } from '../data/data';
+
+const { works, about, contacts } = state;
 
 export default class Bars extends Component {
 
@@ -10,12 +13,12 @@ export default class Bars extends Component {
 
 	render() {
 		
-		let directions = ["about", "works", "contacts"];		
+		let directions = [about, works, contacts];		
 		directions.splice( directions.indexOf(this.props.name), 1)		
 		let buttonClassName =  this.state.show === true ? `${this.props.name}-minimized-modal` : `${this.props.name}-minimized fa fa-bars`; 
 		let button = this.state.show === true ? <Fade top><div className={buttonClassName} onClick={this.openBars}>
-						  <Link tag="h3" direction = {directions[0]} changePage={this.props.changePage}/>
-								<Link tag="h3" direction = {directions[1]} changePage={this.props.changePage}/>
+						  <Link tag="button" direction = {directions[0]} changePage={this.props.changePage}/>
+								<Link tag="button" direction = {directions[1]} changePage={this.props.changePage}/>
 	   							     	<span className="fa fa-times" onClick={this.openBars}></span>
 	   							     	</div> </Fade>  : <div className={buttonClassName} onClick={this.openBars}> </div>
 						     	
